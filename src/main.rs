8c93f71b0eb33b142e42ab8675616472ae3e8d3b
lambdalisue <lambdalisue@hashnote.net>
2020-07-27 21:59:59 +0900

    Add example and initial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+type Field = Vec<Vec<Option<u32>>>;
+
+fn print_field(field: &Field) {
+    for y in 0..9 {
+        for x in 0..9 {
+            if let Some(v) = field[y][x] {
+                print!("{} ", v);
+            } else {
+                print!("  ")
+            }
+        }
+        println!("")
+    }
+}
+
+fn read_line() -> String {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    s.trim_end().to_owned()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut field: Field = vec![vec![None; 9]; 9];
+    for y in 0..9 {
+        for (i, c) in read_line().chars().enumerate() {
+            field[y][i] = c.to_digit(10);
+        }
+    }
+
+    print_field(&field);
 }
